93c0a64cfbcbef74741943a4c2497160afbf2f1a
nunez1822 <nunez1822@yahoo.com>
2016-08-02 23:25:59 -0400

    add testing component

---
+++ src/app/testing/testing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-testing',
+  template: `
+      This is my testing!
+  `
+})
+export class TestingComponent {
+
+}
